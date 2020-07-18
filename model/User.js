36e45be86519886522f9ee80cf58294bbf725b1e
Darryl Mangibin <darrylmangibin@gmail.com>
2020-07-18 11:03:42 +0800

    Add User model

---
+++ model/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+	username: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	updatedAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+const User = mongoose.model("user", UserSchema);
+
+module.exports = User;
