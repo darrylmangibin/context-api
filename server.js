39a9e8b94ff04943da5b68ab1dc653750bb3a697
Darryl Mangibin <darrylmangibin@gmail.com>
2020-07-18 11:10:17 +0800

    Add routes for auth users notes

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 require("./config/db")();
 
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/notes", require("./routes/notes"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(() => console.log(`Server running at port ${PORT}`));
