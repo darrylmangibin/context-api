9ff57f78f80da6b10fe94d3cdc94b025ff605b08
Darryl Mangibin <darrylmangibin@gmail.com>
2020-07-18 11:06:33 +0800

    Add Note model

---
+++ model/Note.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const NoteSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	updatedAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+const Note = mongoose.model("note", NoteSchema);
+
+module.exports = Note;
