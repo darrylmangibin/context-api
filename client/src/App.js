355a475a70138e7c46bc93d14371c83c983c7cdc
Darryl Mangibin <darrylmangibin@gmail.com>
2020-07-18 15:35:15 +0800

    Add routes to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import './styles/main.css';
+import NotesPage from "./pages/NotesPage/";
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
+import AboutPage from './pages/AboutPage';
+
+import "./styles/main.css";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<Router>
+			<Switch>
+				<Route path="/login" exact component={LoginPage} />
+				<Route path="/register" exact component={RegistrationPage} />
+        <Route path="/about" exact component={AboutPage} />
+				<Route component={NotesPage} />
+			</Switch>
+		</Router>
+	);
 };
 
 export default App;
---
+++ client/src/pages/NotesPage/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import AddNotePage from "./AddNotePage";
+import NotesPage from "./NotesPage";
+import PageNotFound from '../../pages/PageNotFound';
+import EditNotePage from "./EditNotePage";
+
+const MainPage = (props) => {
+	console.log(props);
+	return (
+		<Switch>
+			<Route path="/" exact component={NotesPage} />
+			<Route path="/create" exact component={AddNotePage} />
+      <Route path="/edit/:id" exact component={EditNotePage} />
+      <Route path="/" component={PageNotFound} />
+		</Switch>
+	);
+};
+
+export default MainPage;
